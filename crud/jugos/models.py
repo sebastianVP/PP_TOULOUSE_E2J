5fd48460212496a3b0b9142d3d8a7db52be08e31
sebastianVP <alex.valdezp22@gmail.com>
2024-05-02 21:59:53 -0500

    Creacion de un CRUD de la aplicacion JUGOS

---
+++ crud/jugos/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+# Create your models here.
+from django.utils import timezone
+ 
+# Creación de campos de la tabla 'jugos' 
+class Jugos(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.CharField(max_length=20)
+    stock = models.CharField(max_length=100)
+    img = models.FileField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+ 
+    class Meta:
+         db_table = 'jugos' # Le doy de nombre 'jugos' a nuestra tabla en la Base de Datos 
